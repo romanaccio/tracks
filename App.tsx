b0e15ca284fa1a73796a438e5e3fc564ca6493b5
groslapinbleu <antoine.jouannais@gmail.com>
2021-10-14 19:45:57 +0200

    initial commit

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EEE',
     alignItems: 'center',
     justifyContent: 'center',
   },
